c5ab370dbb6fbb112f4ed60498443f04be89ef93
MrTom1k <72761338+tol2k@users.noreply.github.com>
2024-04-12 23:00:00 +0300

    Add files via upload

---
+++ dec.py
@@ -0,0 +1,77 @@
+import types
+
+
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = [x for sublist in list_of_list for x in sublist]
+        self.i = 0
+
+    def __iter__(self):
+
+        return self
+
+    def __next__(self):
+        if self.i < len(self.list_of_list):
+            item = self.list_of_list[self.i]
+            self.i += 1
+            return item
+
+        else:
+            raise StopIteration
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
+
+
+
+def flat_generator(list_of_list):
+
+    for minilist in list_of_list:
+        for item in minilist:
+            yield item
+    
+
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
