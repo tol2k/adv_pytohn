811eb85bb9feeb9a8b3f01c8d9199a72f3e66de8
MrTom1k <72761338+tol2k@users.noreply.github.com>
2024-05-14 21:59:20 +0300

    Add files via upload

---
+++ LIFO.py
@@ -0,0 +1,50 @@
+
+
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+def brackets(string):
+    s = Stack()
+    for i in string:
+        if i in '({[':
+            s.push(i)
+        elif i in ')}]':
+            if s.is_empty():
+                return "Несбалансировано"
+            if i == ")" and s.peek() == "(":
+                s.pop()
+            elif i == "]" and s.peek() == "[":
+                s.pop()
+            elif i == "}" and s.peek() == "{":
+                s.pop()
+            else:
+                return "Несбалансировано"
+    return "Сбалансировано" if s.is_empty() else "Несбалансировано"
+
+
+if __name__ == "__main__":
+    print(brackets('(((([{}]))))'))
+    print(brackets ('{{[(])]}}'))
+
+
+
+
